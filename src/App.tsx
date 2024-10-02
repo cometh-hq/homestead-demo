7f10a79e7a7a550a5ab6160d7e93a31443546d8c
Shoto <38501963+shoto290@users.noreply.github.com>
2024-10-02 09:22:32 +0200

    Update: GitHub link

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     >
       <ConnectKitButton />
       <a
-        href="https://github.com"
+        href="https://github.com/cometh-hq/connectkit-demo"
         style={{
           position: "absolute",
           bottom: 16,
